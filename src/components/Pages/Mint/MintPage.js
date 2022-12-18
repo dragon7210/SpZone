3d807c30b974e353a934aa18e10bca6f261c4a95
dragon <isanoguchi721@gmail.com>
2022-12-18 13:41:29 -0500

    fix

--- src/components/Pages/Mint/MintPage.js
+++ src/components/Pages/Mint/MintPage.js
@@ -4,7 +4,7 @@ import NFTImage from "assets/img/1.png";
 const x = [1, 2, 3];
 const MintPage = () => {
   return (
-    <div>
+    <div className="bg-[wheat] ">
       <div className="flex justify-between px-[20px]">
         {x.map((index, ele) => (
           <Mint key={index} NFTImage={NFTImage} />
